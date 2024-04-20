b2080374e66e458e513373eccca37314a71c5068
Stefan Adrian Danaita <me@dsa.io>
2024-04-20 03:10:24 +0100

    Merge cluster_name into Overview. Link the APIs into the main client

--- src/api/overview.rs
+++ src/api/overview.rs
@@ -1,7 +1,7 @@
 use crate::api::_generic::handle_response;
 use crate::errors::RabbitMqClientError;
 use reqwest_middleware::ClientWithMiddleware;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 pub struct OverviewApi {
     api_url: String,
@@ -20,6 +20,36 @@ impl OverviewApi {
                 reqwest::Method::GET,
                 format!("{}/api/overview", self.api_url),
             )
+            .send()
+            .await?;
+
+        handle_response(response).await
+    }
+
+    pub async fn get_cluster_name(&self) -> Result<RabbitMqClusterName, RabbitMqClientError> {
+        let response = self
+            .client
+            .request(
+                reqwest::Method::GET,
+                format!("{}/api/cluster-name", self.api_url),
+            )
+            .send()
+            .await?;
+
+        handle_response(response).await
+    }
+
+    pub async fn set_cluster_name(
+        &self,
+        request: RabbitMqClusterName,
+    ) -> Result<(), RabbitMqClientError> {
+        let response = self
+            .client
+            .request(
+                reqwest::Method::PUT,
+                format!("{}/api/cluster-name", self.api_url),
+            )
+            .json(&request)
             .send()
             .await?;
 
@@ -91,4 +121,9 @@ pub struct RabbitMqObjectTotals {
     consumers: i64,
     exchanges: i64,
     queues: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RabbitMqClusterName {
+    pub name: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+use crate::api::binding::BindingApi;
+use crate::api::connection::ConnectionApi;
+use crate::api::exchange::ExchangeApi;
+use crate::api::node::NodeApi;
+use crate::api::overview::OverviewApi;
+use crate::api::permission::PermissionApi;
+use crate::api::policy::PolicyApi;
+use crate::api::queue::QueueApi;
+use crate::api::user::UserApi;
+use crate::api::vhost::VhostApi;
 use crate::config::RabbitMqClientConfig;
 use crate::errors::RabbitMqClientError;
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
@@ -10,6 +20,20 @@ mod middlewares;
 pub struct RabbitMqClient {
     pub api_url: String,
     pub client: ClientWithMiddleware,
+    pub apis: RabbitMqApis,
+}
+
+pub struct RabbitMqApis {
+    bindings: BindingApi,
+    connections: ConnectionApi,
+    exchanges: ExchangeApi,
+    nodes: NodeApi,
+    overview: OverviewApi,
+    permissions: PermissionApi,
+    policies: PolicyApi,
+    queues: QueueApi,
+    users: UserApi,
+    vhosts: VhostApi,
 }
 
 pub struct RabbitMqClientBuilder {
@@ -36,8 +60,24 @@ impl RabbitMqClientBuilder {
             .unwrap_or_else(|| ClientBuilder::new(reqwest::Client::new()).build());
 
         Ok(RabbitMqClient {
-            client,
+            apis: build_apis(self.config.rabbitmq_api_url.clone(), client.clone()),
             api_url: self.config.rabbitmq_api_url,
+            client,
         })
+    }
+}
+
+fn build_apis(url: String, client: ClientWithMiddleware) -> RabbitMqApis {
+    RabbitMqApis {
+        bindings: BindingApi::new(url.clone(), client.clone()),
+        connections: ConnectionApi::new(url.clone(), client.clone()),
+        exchanges: ExchangeApi::new(url.clone(), client.clone()),
+        nodes: NodeApi::new(url.clone(), client.clone()),
+        overview: OverviewApi::new(url.clone(), client.clone()),
+        permissions: PermissionApi::new(url.clone(), client.clone()),
+        policies: PolicyApi::new(url.clone(), client.clone()),
+        queues: QueueApi::new(url.clone(), client.clone()),
+        users: UserApi::new(url.clone(), client.clone()),
+        vhosts: VhostApi::new(url.clone(), client.clone()),
     }
 }
